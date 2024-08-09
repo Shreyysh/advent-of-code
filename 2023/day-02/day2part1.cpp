15e7f03ab9b5200c400d860693ccdb06b2873cb4
Shreyas Sharma <shreyas.sharma1019@gmail.com>
2024-08-09 22:57:08 +0530

    added a comment

--- 2023/day-02/day2part1.cpp
+++ 2023/day-02/day2part1.cpp
@@ -1,3 +1,4 @@
+//elf takes out cubes THEN PUTS IT BACK AGAIN
 //Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 
 #include <iostream>
